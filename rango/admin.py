627527224f3119ab3eb76e3711bb1be6d82ef06d
Raphael Memmi <2563174m@student.gla.ac.uk>
2022-02-08 22:51:20 +0000

    Chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -8,6 +8,3 @@ admin.site.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page, PageAdmin)
-admin.site.register(UserProfile)
